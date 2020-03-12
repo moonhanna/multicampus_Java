d849ca986357b10a41630e3384a5fd2230aa9447
moonhanna <mhn97@naver.com>
2020-03-12 20:18:52 +0900

    update day12

---
+++ network/day12/mul2020/app/src/main/java/com/example/mul_2020/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mul_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
